0bac4027525d9f7bc35ae3ac71691af9100acb44
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2024-02-12 12:13:50 +0100

    First part of Day 01 works

--- src/main/kotlin/aoc2023/day01.kt
+++ src/main/kotlin/aoc2023/day01.kt
@@ -4,8 +4,19 @@ import utils.readInput
 
 fun main() {
     val inputs = readInput("2023", "day01")
-    //check if file acces is ok, and algo in Aoc2023Utils.kt works
-    inputs.forEach {line ->
-        println(line)
-    }
+    println("la somme est : ${solvePartOne(inputs)}")
 }
+
+fun solvePartOne(inputs: List<String>): Int {
+    return inputs.sumOf {line->
+        val numbers = line.filter {
+            it.isDigit()
+        }
+        val firstDigit = numbers.first()
+        val lastDigit = numbers.lastIndex?:firstDigit
+        "$firstDigit$lastDigit".toInt()
+    }
+
+}
+
+
